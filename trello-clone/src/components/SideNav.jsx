9ffff7bfccdeb72de9b77afc45d93e75f933949b
OkDaGyeong <81674153+OkDaGyeong@users.noreply.github.com>
2023-06-15 10:01:47 +0900

    .

--- trello-clone/src/components/SideNav.jsx
+++ trello-clone/src/components/SideNav.jsx
@@ -15,16 +15,28 @@ function SideNav() {
       {/* 사이드 메뉴*/}
 
       <div className="sidebar-container">
-        <button id="menu-toggle" onClick={handleToggle}>
-          메뉴 열기/닫기
-        </button>
+        <div className="user-box">
+          <div id="user-profile">U</div>
+          <div id="user-name">user ID</div>
+          <button id="menu-toggle" onClick={handleToggle}>
+            <img
+              src="https://a.trellocdn.com/prgb/assets/58243262833f693f6101.svg"
+              alt="버튼"
+            />
+          </button>
+        </div>
+
         <nav className={`sidebar ${isOpen ? "open" : ""}`}>
-          <ul>
+          <ul className="menu-list">
             <li>
-              <Link to="/">Home</Link>
+              <Link className="menu-item" to="/">
+                Home
+              </Link>
             </li>
             <li>
-              <Link to="/login">Login</Link>
+              <Link className="menu-item" to="/login">
+                Login
+              </Link>
             </li>
           </ul>
         </nav>
